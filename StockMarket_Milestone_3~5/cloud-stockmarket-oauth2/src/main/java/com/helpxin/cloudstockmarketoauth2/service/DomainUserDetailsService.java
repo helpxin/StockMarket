42951654e506d48d7b23ea434d58f7754bc84859
helpxin <133448550@qq.com>
2020-06-30 03:47:27 +0800

    Milestone5_UPDATE

---
+++ StockMarket_Milestone_3~5/cloud-stockmarket-oauth2/src/main/java/com/helpxin/cloudstockmarketoauth2/service/DomainUserDetailsService.java
@@ -0,0 +1,71 @@
+package com.helpxin.cloudstockmarketoauth2.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+
+/**
+ * Created by sx
+ * on 2020/6/18
+ */
+@Component
+@Slf4j
+public class DomainUserDetailsService implements UserDetailsService {
+
+//    @Resource
+//    private GoddessUserService userService ;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+//        GoddessUser goddessUser = userService.getOne(new QueryWrapper<GoddessUser>().eq("user_name", s));
+//        log.info("user:"+goddessUser.getUsername()+" login success!");
+//        return goddessUser;
+//    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return null;
+            }
+
+            @Override
+            public String getUsername() {
+                return null;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return false;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+        };
+    }
+}
